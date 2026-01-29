be595c261c04781c738e927a3ad7461750a46bd2
AlbaDuqueC <alba.duque@iesnervion.es>
2026-01-29 10:11:36 +0100

    SpringBoot

---
+++ PruebaSpringBoot/src/main/java/com/ejemplo/mi_app/MiAppApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.ejemplo.mi_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiAppApplication.class, args);
+	}
+
+}
